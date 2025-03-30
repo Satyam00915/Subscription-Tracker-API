1d624ce313d26ed9d441acfa745a97e371ec10de
Satyam00915 <satyamupadhyay0495@gmail.com>
2025-03-30 17:35:05 +0530

    error middleware added

--- app.js
+++ app.js
@@ -4,15 +4,21 @@ import authRouter from "./Routes/auth.routes.js";
 import userRouter from "./Routes/user.routes.js";
 import subscriptionRouter from "./Routes/subscription.routes.js";
 import connectToDatabase from "./DATABASE/mongodb.js";
+import errorMiddleware from "./middleware/error.middleware.js";
+import cookieParser from "cookie-parser";
 
 const app = express();
 
 app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
 
 app.use("/api/v1/auth", authRouter);
 app.use("/api/v1/users", userRouter);
 app.use("/api/v1/subscription", subscriptionRouter);
 
+//Ensuring all the errors from route handler get caught
+app.use(errorMiddleware);
 
 app.listen(3000, async () => {
   console.log(`Listening on Server ${PORT}`);
